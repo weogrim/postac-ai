19e1e5dd03ecc75bcb1d813cf839350537b54273
Darek <dariusz.chowanski@gmail.com>
2026-04-27 20:28:00 +0200

    fix deprecated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->validateCsrfTokens(except: [
+        $middleware->preventRequestForgery(except: [
             'stripe/webhook',
         ]);
     })
